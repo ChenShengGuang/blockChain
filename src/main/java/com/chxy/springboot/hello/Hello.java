778864d1a4b570cfe31b9dc26130fb1fade4252b
bigPoss <2078422241@qq.com>
2019-05-05 20:54:07 +0800

    20:53修改

--- src/main/java/com/chxy/springboot/hello/Hello.java
+++ src/main/java/com/chxy/springboot/hello/Hello.java
@@ -2,6 +2,6 @@ package com.chxy.springboot.hello;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("sunwuk ");
+        System.out.println(" 猪八戒");
     }
 }
