4f4f532a83af6528e0fd9eee1e42bf75d4048c42
bigPoss <2078422241@qq.com>
2019-05-16 22:31:10 +0800

    22:30

--- src/main/java/com/chxy/springboot/hello/Hello.java
+++ src/main/java/com/chxy/springboot/hello/Hello.java
@@ -3,5 +3,6 @@ package com.chxy.springboot.hello;
 public class Hello {
     public static void main(String[] args) {
         System.out.println(" 猪八戒");
+        System.out.println("孙悟空");
     }
 }
