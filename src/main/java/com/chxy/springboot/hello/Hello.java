d20857fcfe37b8e78a9a374622551328e8e9237b
ChenShengGuang <46028856+ChenShengGuang@users.noreply.github.com>
2019-05-16 22:30:37 +0800

    Update Hello.java

--- src/main/java/com/chxy/springboot/hello/Hello.java
+++ src/main/java/com/chxy/springboot/hello/Hello.java
@@ -2,7 +2,8 @@ package com.chxy.springboot.hello;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println(" 猪八戒");
+        System.out.println(" 猪八戒.........");
+      
         System.out.println("孙悟空");
     }
 }
